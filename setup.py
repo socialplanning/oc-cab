9ea50010731dee575e08d89d8fa7c12ad7fa1709
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2007-11-30 23:11:11 +0000

    initial import
    git-svn-id: https://svn.socialplanning.org/svn/OpencoreTaskTracker/trunk@11519 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(name='OpencoreTaskTracker',
+      version=version,
+      description="tasktracker yay",
+      long_description="""\
+""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='me',
+      author_email='',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['opencore'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
