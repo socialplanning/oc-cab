718c8c6695d6df62d0cbec58985d7998b247c3eb
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2008-03-02 17:28:04 +0000

    add setup step
    git-svn-id: https://svn.socialplanning.org/svn/oc-cab@13919 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ trunk/opencore/cabochon/configuration/setuphandlers.py
@@ -0,0 +1,20 @@
+import logging
+from opencore.configuration.setuphandlers import setuphandler
+from opencore.configuration.setuphandlers import register_local_utility
+from opencore.cabochon.interfaces import ICabochonClient
+from opencore.cabochon.client import CabochonConfigError
+from opencore.cabochon.client import CabochonUtility
+
+logger = logging.getLogger('opencore.cabochon.configuration.setuphandlers')
+
+@setuphandler
+def install_cabochon_utility(portal, out):
+    try:
+        factory_fn = lambda:CabochonUtility(portal)
+        register_local_utility(portal, out,
+                               ICabochonClient, CabochonUtility,
+                               factory_fn)
+    except ValueError:
+        logger.info(traceback.print_exc())
+    except CabochonConfigError:
+        logger.info(traceback.print_exc())
