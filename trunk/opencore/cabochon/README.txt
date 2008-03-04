ea45bbaa8764fa225dcb7b7ebe53c8bf00785ed0
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2008-03-04 18:37:33 +0000

    empty tests
    git-svn-id: https://svn.socialplanning.org/svn/oc-cab@13984 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ trunk/opencore/cabochon/README.txt
@@ -0,0 +1,10 @@
+=================
+opencore.cabochon
+=================
+
+opencore.cabochon is opencore's helper package to CabochonClient.
+
+And this is an empty test file.
+
+    >>> 1 == 0
+    False
---
+++ trunk/opencore/cabochon/tests.py
@@ -0,0 +1,27 @@
+from Products.PloneTestCase.PloneTestCase import FunctionalTestCase
+from opencore.testing import dtfactory as dtf
+from opencore.testing.layer import MockHTTPWithContent
+from zope.testing import doctest
+import unittest
+
+
+optionflags = doctest.ELLIPSIS
+
+import warnings; warnings.filterwarnings("ignore")
+
+def test_suite():
+
+    globs = locals()
+    readme = dtf.ZopeDocFileSuite("README.txt", 
+                                    optionflags=optionflags,
+                                    package='opencore.cabochon',
+                                    test_class=FunctionalTestCase,
+                                    globs = globs,
+                                    layer = MockHTTPWithContent,
+                                    )
+
+    suites = (readme,)
+    return unittest.TestSuite(suites)
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
