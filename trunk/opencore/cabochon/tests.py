0469376038a6982c848c54fc9e0d4b02f034b8b3
Rob Marianski <rob@marianski.com>
2008-03-12 15:21:44 +0000

    make cabochon utility a global utility now that configuration can be read from build.ini
    git-svn-id: https://svn.socialplanning.org/svn/oc-cab@14379 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- trunk/opencore/cabochon/tests.py
+++ trunk/opencore/cabochon/tests.py
@@ -1,8 +1,6 @@
-from Products.PloneTestCase.PloneTestCase import FunctionalTestCase
-from opencore.testing import dtfactory as dtf
-from opencore.testing.layer import MockHTTPWithContent
 from zope.testing import doctest
 import unittest
+import doctest
 
 
 optionflags = doctest.ELLIPSIS
@@ -12,16 +10,13 @@ import warnings; warnings.filterwarnings("ignore")
 def test_suite():
 
     from pprint import pprint
-    from opencore.cabochon.testing.utility import pop_test_log
 
     globs = locals()
 
-    readme = dtf.ZopeDocFileSuite("README.txt", 
+    readme = doctest.DocFileSuite("README.txt", 
                                   optionflags=optionflags,
                                   package='opencore.cabochon',
-                                  test_class=FunctionalTestCase,
                                   globs = globs,
-                                  layer = MockHTTPWithContent,
                                   )
 
     suites = (readme,)
