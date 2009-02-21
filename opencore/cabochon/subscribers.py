02114dd4f949147351381ae0cdd4ed60b8cbff14
Rob Miller <rob@kalistra.com>
2009-02-21 08:46:32 +0000

    event listener refactoring: - create once_per_request decorator and use it to wrap subscribers - merge wikipage edit and add subscribers into one
    git-svn-id: https://svn.socialplanning.org/svn/opencore/plugins/oc-cab/branches/sputnik@22953 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/cabochon/subscribers.py
+++ opencore/cabochon/subscribers.py
@@ -6,30 +6,25 @@ from opencore.project.browser.projectinfo import ProjectInfo
 from Products.CMFCore.utils import getToolByName
 from zope.app.container.contained import IObjectRemovedEvent
 from zope.app.container.contained import IObjectAddedEvent
-from zope.app.event.interfaces import IObjectModifiedEvent
 from zope.component import adapter
 from zope.component import getUtility
 
 
-@adapter(IProject, IObjectRemovedEvent)
-def notify_project_deleted(project, event=None):
-    # project info passed to cabochon
-    id = project.getId()
+def once_per_request(subscriber_fn):
+    def inner(obj, event=None):
+        # sucks to use acquisition here, but we don't have a
+        # get_request function yet
+        request = obj.REQUEST
+        if getattr(request, '_cab_already_notified', False):
+            return
+        request._cab_already_notified = True
+        return subscriber_fn(obj, event)
+    return inner
+        
 
-    cabochon_utility = getUtility(ICabochonClient)
-    cabochon_utility.notify_project_deleted(id)
-
-
-@adapter(IOpenPage, IObjectAddedEvent)
-def notify_wikipage_created(page, event=None):
-    # mark the request object so we don't fire more than one event per
-    # request; sucks to use acquisition here, but we don't have a
-    # get_request function yet
-    request = page.REQUEST
-    if getattr(request, '_cab_already_notified', False):
-        return
-    request._cab_already_notified = True
-    parent = event.newParent
+@once_per_request
+def wikipage_notifier(page, event=None):
+    project = ProjectInfo(page).project
     url = page.absolute_url()
     mtool = getToolByName(page, 'portal_membership')
     member = mtool.getAuthenticatedMember()
@@ -37,22 +32,26 @@ def notify_wikipage_created(page, event=None):
     title = page.title_or_id()
     updated = datetime_to_string(datetime.now())
     object_type = 'page'
-    action = 'created'
-    project = ProjectInfo(page).project
+    action = 'modified'
+    if IObjectAddedEvent.providedBy(event):
+        action = 'created'
     closed = 0
     categories = ['wiki']
+    summary = "Wiki page %s: '%s'" % (action, title)
 
     feed_kwargs = dict(link=url)
     if project is not None:
         proj_id = project.getId()
         feed_kwargs['project'] = proj_id
         categories.append('projects/%s' % proj_id)
+        summary += " (in project '%s')" % project.title_or_id()
         wfp = project._getOb('.wf_policy_config', None)
         if wfp is not None:
             policy = wfp.getPolicyIn().getId()
             if policy.startswith('closed'):
                 closed = 1
 
+    feed_kwargs['summary'] = summary
     feed_kwargs['closed'] = closed
     feed_kwargs['categories'] = categories
 
@@ -60,9 +59,20 @@ def notify_wikipage_created(page, event=None):
     cabochon_utility.send_feed_item(page.getId(), object_type, action, title,
                                     updated, username, **feed_kwargs)
 
-    # XXX older-style notifier, can we refactor these so we don't have so
-    # many methods?
-    cabochon_utility.notify_wikipage_created(parent, title, url, username)
+    # BBB older-style notifiers, these really need to go away,
+    # requires refactoring listeners that expect these events to be
+    # fired.  i _think_ this is only 
+    if action == 'created':
+        parent = event.newParent
+        cabochon_utility.notify_wikipage_created(parent, title, url, username)
+    else:
+        # modified
+        if project is not None:
+            cabochon_utility.notify_wikipage_updated(project,
+                                                     title,
+                                                     url,
+                                                     username)
+
 
 @adapter(IOpenPage, IObjectRemovedEvent)
 def notify_wikipage_deleted(page, event=None):
@@ -79,52 +89,10 @@ def notify_wikipage_deleted(page, event=None):
     cabochon_utility.notify_wikipage_deleted(page_url, username)
 
 
-@adapter(IOpenPage, IObjectModifiedEvent)
-def notify_wikipage_modified(page, event=None):
-    # mark the request object so we don't fire more than one event per
-    # request; sucks to use acquisition here, but we don't have a
-    # get_request function yet
-    request = page.REQUEST
-    if getattr(request, '_cab_already_notified', False):
-        return
-    request._cab_already_notified = True
-
-    project = ProjectInfo(page).project
-    url = page.absolute_url()
-    mtool = getToolByName(page, 'portal_membership')
-    member = mtool.getAuthenticatedMember()
-    username = member.getId()
-    title = page.title_or_id()
-    updated = datetime_to_string(datetime.now())
-    object_type = 'page'
-    action = 'modified'
-    closed = 0
-    categories = ['wiki']
-    summary = "Wiki page '%s' edited" % title
-
-    feed_kwargs = dict(link=url)
-    if project is not None:
-        proj_id = project.getId()
-        feed_kwargs['project'] = proj_id
-        categories.append('projects/%s' % proj_id)
-        summary += " (in project '%s')" % project.title_or_id()
-        wfp = project._getOb('.wf_policy_config', None)
-        if wfp is not None:
-            policy = wfp.getPolicyIn().getId()
-            if policy.startswith('closed'):
-                closed = 1
-
-    feed_kwargs['closed'] = closed
-    feed_kwargs['categories'] = categories
-    feed_kwargs['summary'] = summary
+@adapter(IProject, IObjectRemovedEvent)
+def notify_project_deleted(project, event=None):
+    # project info passed to cabochon
+    id = project.getId()
 
     cabochon_utility = getUtility(ICabochonClient)
-    cabochon_utility.send_feed_item(page.getId(), object_type, action, title,
-                                    updated, username, **feed_kwargs)
-
-    # older style notification; we should refactor these
-    if project is not None:
-        cabochon_utility.notify_wikipage_updated(project,
-                                                 title,
-                                                 url,
-                                                 username)
+    cabochon_utility.notify_project_deleted(id)
