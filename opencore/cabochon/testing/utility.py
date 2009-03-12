c647c3cd3c4496e519c58d0637ba6e3a0f265542
Rob Miller <rob@kalistra.com>
2009-03-12 21:42:06 +0000

    clean up mock cabochon client utility for unit tests
    git-svn-id: https://svn.socialplanning.org/svn/opencore/plugins/oc-cab/branches/sputnik@23277 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/cabochon/testing/utility.py
+++ opencore/cabochon/testing/utility.py
@@ -1,5 +1,6 @@
 from opencore.cabochon.interfaces import ICabochonClient
-from zope.component import queryUtility
+from opencore.testing.minimock import Mock
+from opencore.testing.utility import mock_utility
 from zope.component import provideUtility
 from zope.interface import implements
 
@@ -9,20 +10,26 @@ def pop_test_log():
     if test_log:
         return test_log.pop()
 
-def setup_cabochon_mock(portal):
-    if queryUtility(ICabochonClient):
-        provideUtility(StubCabochonClient(), ICabochonClient)
 
 # why not use minimock?
-class StubCabochonClient(object):
+class StubCabochonClient(Mock):
     """stub class used to monkey patch cabochon for unit tests"""
     implements(ICabochonClient)
 
-    def _stub(self, *args):
-        test_log.append('opencore.testing.utility.StubCabochonClient: args: %s' % (args,))
+    def _stub(self, *args, **kwargs):
+        argstr = str(args)
+        kwargstr = str(kwargs)
+        test_log.append('opencore.testing.utility.StubCabochonClient: args: %s; kwargs: %s' % (argstr, kwargstr))
 
+    send_feed_item = _stub
     notify_project_created = _stub
     notify_project_deleted = _stub
     notify_wikipage_created = _stub
     notify_wikipage_updated = _stub
     notify_wikipage_deleted = _stub
+
+
+def setup_cabochon_mock():
+    cabclient = mock_utility('opencore.cabochon.client.CabochonClient',
+                             ICabochonClient, StubCabochonClient)
+    provideUtility(cabclient, provides=ICabochonClient)
