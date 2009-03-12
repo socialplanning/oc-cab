5258702d3b4d4dadbc8a39dbb3e93b48249f0ea6
Rob Miller <rob@kalistra.com>
2009-03-12 21:41:24 +0000

    add send_feed_item method to ICabochonClient interface and remove 'self' from the interface method declarations
    git-svn-id: https://svn.socialplanning.org/svn/opencore/plugins/oc-cab/branches/sputnik@23276 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/cabochon/interfaces.py
+++ opencore/cabochon/interfaces.py
@@ -4,17 +4,21 @@ class ICabochonClient(Interface):
     def __init__(self, conf):
         pass
 
-    def notify_project_created(self, id, creator):
+    def send_feed_item(id, obtype, action, title, updated, author_map,
+                       **kwargs):
+        """send feed item data to feedbacker"""
+
+    def notify_project_created(id, creator):
         """send a project created message to cabochon"""
 
-    def notify_project_deleted(self, id):
+    def notify_project_deleted(id):
         """send a project deleted message to cabochon"""
 
-    def notify_wikipage_created(self, project, page_title, page_url, creatorid):
+    def notify_wikipage_created(project, page_title, page_url, creatorid):
         """Send a page created message to cabochon"""
 
-    def notify_wikipage_updated(self, project, page_title, page_url, userid):
+    def notify_wikipage_updated(project, page_title, page_url, userid):
         """Send a page updated message to cabochon"""
 
-    def notify_wikipage_deleted(self, page_url, userid):
+    def notify_wikipage_deleted(page_url, userid):
         """Send a page updated message to cabochon"""
